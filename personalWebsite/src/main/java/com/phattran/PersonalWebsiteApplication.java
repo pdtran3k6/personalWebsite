5eb2291541a51b59253555f7173c953ddffd2c9a
Phat Tran <phatdtran3k6@gmail.com>
2016-01-16 23:08:42 -0500

    init commit

---
+++ personalWebsite/src/main/java/com/phattran/PersonalWebsiteApplication.java
@@ -0,0 +1,28 @@
+package com.phattran;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class PersonalWebsiteApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(applicationClass, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(applicationClass);
+    }
+
+    private static Class<PersonalWebsiteApplication> applicationClass = PersonalWebsiteApplication.class;
+}
---
+++ personalWebsite/src/test/java/com/phattran/PersonalWebsiteApplicationTests.java
@@ -0,0 +1,18 @@
+package com.phattran;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = PersonalWebsiteApplication.class)
+@WebAppConfiguration
+public class PersonalWebsiteApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

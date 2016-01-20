cd5b3ff7d5a28a0ce58f4bff7675408656550f54
Phat Tran <phatdtran3k6@gmail.com>
2016-01-19 22:34:02 -0500

    Fix up the linkage between pages Add home link to every pages

--- personalWebsite/src/main/java/com/phattran/MvcConfig.java
+++ personalWebsite/src/main/java/com/phattran/MvcConfig.java
@@ -10,6 +10,10 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/aboutme").setViewName("aboutme");
+        registry.addViewController("/projects").setViewName("projects");
+        registry.addViewController("/contact").setViewName("contact");
+        registry.addViewController("/home").setViewName("home");
         registry.addViewController("/").setViewName("home");
         registry.addViewController("/hello").setViewName("hello");
         registry.addViewController("/login").setViewName("login");

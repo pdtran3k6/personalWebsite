921bd1256951e94545b1930021b20ea796c235da
Phat Tran <phatdtran3k6@gmail.com>
2016-02-07 19:15:05 -0500

    Changed background and navbar background; Fixed linkage between html pages; Added new page 'Articles'; Prevented background from repeating

--- src/main/java/com/phattran/MvcConfig.java
+++ src/main/java/com/phattran/MvcConfig.java
@@ -9,14 +9,11 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/home").setViewName("home");
         registry.addViewController("/aboutme").setViewName("aboutme");
         registry.addViewController("/projects").setViewName("projects");
-        registry.addViewController("/contact").setViewName("contact");
+        registry.addViewController("/articles").setViewName("articles");
         registry.addViewController("/home").setViewName("home");
         registry.addViewController("/").setViewName("home");
-        registry.addViewController("/hello").setViewName("hello");
-        registry.addViewController("/login").setViewName("login");
     }
 
 }

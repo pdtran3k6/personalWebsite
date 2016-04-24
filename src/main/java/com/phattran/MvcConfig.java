6712ff60e2c9b75ba1932f14589e1f971ad6e0bb
Phat Tran <phatdtran3k6@gmail.com>
2016-04-23 21:44:18 -0400

    Changed layout of aboutme.html Removed unnecessary sites Cleaned up style.css

--- src/main/java/com/phattran/MvcConfig.java
+++ src/main/java/com/phattran/MvcConfig.java
@@ -9,14 +9,9 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/aboutme").setViewName("aboutme");
+    	registry.addViewController("/").setViewName("aboutme");
+    	registry.addViewController("/aboutme").setViewName("aboutme");
         registry.addViewController("/projects").setViewName("projects");
-        registry.addViewController("/articles").setViewName("articles");
-        registry.addViewController("/home").setViewName("home");
-        registry.addViewController("/").setViewName("home");
-        
-        // Articles linkage
-        registry.addViewController("/articles/how-to-get-better-at-StarCraft").setViewName("/articles/how-to-get-better-at-StarCraft");
     }
 
 }
